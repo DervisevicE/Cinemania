b1712a4f08011559f62a60b1bb8902ada4854b46
DervisevicE <edervisevi2@etf.unsa.ba>
2023-07-16 12:37:52 +0200

    Working on home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native';
+import { Bars3BottomLeftIcon, Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 
 export default function HomeScreen() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Open up App.js to start working on your appsssssss!</Text>
+
+            <SafeAreaView style={styles.headerContainer}>
+                <StatusBar style='light' />
+                <View style={styles.header}>
+                    <Bars3CenterLeftIcon size={30} strokeWidth={2} color="white" marginLeft={12} />
+                    <Text style={styles.text}>Cinemania</Text>
+                    <TouchableOpacity>
+                        <MagnifyingGlassIcon size={28} strokeWidth={2} color="white" marginRight={12} />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {/* trending movies */}
+            </ScrollView>
+
+
         </SafeAreaView>
     );
 }
@@ -14,10 +32,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#333333',
-        marginTop: 80,
     },
     text: {
         color: "white",
-        marginTop: 8
-    }
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    headerContainer: {
+        marginTop: 80
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
 });
